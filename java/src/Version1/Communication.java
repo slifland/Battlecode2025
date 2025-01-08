6e6a9ddcc9c4f804c1f1a40c4801eba5d12eb94e
uravtanna <uravtanna@gmail.com>
2025-01-07 19:19:48 -0500

    created ruie and communication class

---
+++ java/src/Version1/Communication.java
@@ -0,0 +1,14 @@
+package Version1;
+
+import battlecode.common.RobotController;
+
+public class Communication
+{
+    /*
+    Scan all rune locations and send them to a nearby tower
+    */
+    public static void sendRuneLocationToTower(RobotController rc)
+    {
+
+    }
+}
---
+++ java/src/Version1/Ruin.java
@@ -0,0 +1,9 @@
+package Version1;
+
+import battlecode.common.*;
+
+public class Ruin
+{
+    public MapLocation location;
+    public int status;              //0 - empty, 1 - ally, 2 - enemy
+}
