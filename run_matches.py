97f12ac79b60265928d263c8537207c29c17add3
slifland <97201326+slifland@users.noreply.github.com>
2025-01-20 14:44:46 -0500

    Update run_matches.py

--- run_matches.py
+++ run_matches.py
@@ -1,303 +1,101 @@
-import math
+from itertools import product
 import subprocess
-import sys
-from argparse import ArgumentParser
-from collections import Counter
-from dataclasses import dataclass, field
-from datetime import datetime
-from multiprocessing.pool import ThreadPool
-from pathlib import Path
-from rich.console import Console
-from rich.segment import Segments
-from rich.table import Table
-from rich.text import Text
 
-@dataclass
-class Match:
-    map: str
-    reverse: bool
-    player1_wins: bool
-    win_condition: str
-    replay_name: str
+emojiMode = False
+emojiMap = {
+    'Won': ':heavy_check_mark:',
+    'Lost': ':x:',
+    'Tied': ':grimacing:',
+    'N/A': ':heavy_minus_sign:',
+    'Error': ':heavy_exclamation_mark:'
+}
+errors = []
+currentBot = 'Version10'
 
-    @property
-    def win_condition_short(self) -> str:
-        return {
-            "The winning team captured all flags.": "Flags",
-            "The winning team won on tiebreakers (captured more flags).": "T Flags Captured",
-            "The winning team won on tiebreakers (higher sum of all unit levels).": "T Unit Levels",
-            "The winning team won on tiebreakers (more crumbs)": "T Crumbs",
-            "The winning team won on tiebreakers (more flags picked up).": "T Flags Picked Up",
-            "The winning team won arbitrarily (coin flip).": "T Random",
-            "Other team has resigned. Congrats on scaring them I guess...": "Resigned"
-        }.get(self.win_condition, self.win_condition)
+bots = ['Version9', 'Version8', 'Version6']
+botsSet = set(bots)
+maps = ['DefaultSmall', 'DefaultLarge' , 'DefaultHuge', 'Money', 'Mirage', 'Justice', 'gardenworld', 'memstore', 'Thirds', 'TargetPractice']
+mapsSet = set(maps)
 
-@dataclass
-class PlayerStatistics:
-    wins: int = 0
-    win_maps: int = 0
-    draw_maps: int = 0
-    lose_maps: int = 0
-    wins_by_condition: Counter = field(default_factory=lambda: Counter())
+matches = set(product(bots, maps))
 
-@dataclass
-class State:
-    player1: str
-    player2: str
-    maps: list[str]
-    matches: list[Match]
-    console: Console
-    timestamp: str
+numWinsMapping = {
+    0: 'Lost',
+    1: 'Tied',
+    2: 'Won',
+}
 
-    def print(self) -> None:
-        maps_table = self.get_maps_table()
-        stats_table = self.get_stats_table()
 
-        maps_segments = list(maps_table.__rich_console__(self.console, self.console.options))
-        stats_segments = list(stats_table.__rich_console__(self.console, self.console.options))
+def retrieveGameLength(output):
+    startIndex = output.find('wins (round ')
+    if startIndex == -1:
+        return -1
+    endIndex = output.find(')', startIndex)
+    if endIndex == -1:
+        return -1
+    return output[startIndex + len('wins(round ') + 1:endIndex]
 
-        maps_footer = maps_segments[-2].text
-        stats_header = stats_segments[0].text
-
-        separator = "┢"
-
-        separator_length = max(len(maps_footer), len(stats_header))
-        for i in range(1, separator_length):
-            has_next = i < separator_length - 1
-            has_up = i < len(maps_footer) and maps_footer[i] in "┴┘"
-            has_down = i < len(stats_header) and stats_header[i] in "┳┓"
-
-            if has_next and not has_up and not has_down:
-                separator += "━"
-            elif has_next and has_up and not has_down:
-                separator += "┷"
-            elif has_next and not has_up and has_down:
-                separator += "┳"
-            elif has_next and has_up and has_down:
-                separator += "╈"
-            elif not has_next and has_up and not has_down:
-                separator += "┙"
-            elif not has_next and not has_up and has_down:
-                separator += "┓"
-            elif not has_next and has_up and has_down:
-                separator += "┪"
-
-        self.console.print(Segments([*maps_segments[:-2], Text(separator, style="bold"), *stats_segments[2:]]))
-
-    def get_maps_table(self) -> Table:
-        table = Table()
-
-        column_groups = 4
-        for _ in range(column_groups):
-            for column in ["Map", "1", "2"]:
-                table.add_column(column, style="bold" if column == "Map" else None, overflow="fold")
-
-        row_count = math.ceil(len(self.maps) / column_groups)
-        for y in range(row_count):
-            row = []
-
-            for x in range(column_groups):
-                map_idx = x * row_count + y
-                if map_idx >= len(self.maps):
-                    row.extend([""] * 3)
-                    continue
-
-                map = self.maps[map_idx]
-                wins = 0
-                losses = 0
-
-                for reverse in [False, True]:
-                    match = next((match for match in self.matches if match.map == map and match.reverse == reverse), None)
-                    if match is None:
-                        row.append("")
-                    elif match.player1_wins != reverse:
-                        row.append(f"[green][link={self.get_viewer_url(match.replay_name)}]{match.win_condition_short}[/][/]")
-                        wins += 1
-                    else:
-                        row.append(f"[red][link={self.get_viewer_url(match.replay_name)}]{match.win_condition_short}[/][/]")
-                        losses += 1
-
-                if wins == 2:
-                    map_color = "green"
-                elif losses == 2:
-                    map_color = "red"
-                elif wins + losses == 2:
-                    map_color = "yellow"
-                else:
-                    map_color = None
-
-                map_prefix = f"[{map_color}]" if map_color is not None else ""
-                map_suffix = "[/]" if map_color is not None else ""
-
-                row.insert(-2, map_prefix + map + map_suffix)
-
-            table.add_row(*row)
-
-        return table
-
-    def get_stats_table(self) -> Table:
-        table = Table()
-
-        table.add_column("Player", style="bold", overflow="fold")
-        table.add_column("Results", overflow="fold")
-        table.add_column("Win rate", overflow="fold")
-        table.add_column("Maps", overflow="fold")
-
-        player1_stats = PlayerStatistics()
-        player2_stats = PlayerStatistics()
-
-        for map in self.maps:
-            wins = 0
-            losses = 0
-
-            for reverse in [False, True]:
-                match = next((match for match in self.matches if match.map == map and match.reverse == reverse), None)
-                if match is None:
-                    continue
-
-                if match.player1_wins != reverse:
-                    wins += 1
-                    player1_stats.wins += 1
-                    player1_stats.wins_by_condition[match.win_condition_short] += 1
-                else:
-                    losses += 1
-                    player2_stats.wins += 1
-                    player2_stats.wins_by_condition[match.win_condition_short] += 1
-
-            if wins == 2:
-                player1_stats.win_maps += 1
-                player2_stats.lose_maps += 1
-            elif losses == 2:
-                player1_stats.lose_maps += 1
-                player2_stats.win_maps += 1
-            elif wins + losses == 2:
-                player1_stats.draw_maps += 1
-                player2_stats.draw_maps += 1
-
-        conditions = set(player1_stats.wins_by_condition.keys()) | set(player2_stats.wins_by_condition.keys())
-        conditions = sorted(conditions, key=lambda c: player1_stats.wins_by_condition[c] + player2_stats.wins_by_condition[c], reverse=True)
-        for condition in conditions:
-            table.add_column(condition, overflow="fold")
-
-        match_count = len(self.matches)
-
-        for name, stats, other_stats in [
-            (self.player1, player1_stats, player2_stats),
-            (self.player2, player2_stats, player1_stats)
-        ]:
-            row = [
-                name,
-                f"[green]{stats.wins}[/]/[red]{match_count - stats.wins}[/]",
-                f"{stats.wins / match_count * 100 if match_count > 0 else 0:,.2f}%",
-                f"[green]{stats.win_maps}[/]/[yellow]{stats.draw_maps}[/]/[red]{stats.lose_maps}[/]"
-            ]
-
-            for condition in conditions:
-                wins = stats.wins_by_condition[condition]
-                losses = other_stats.wins_by_condition[condition]
-                row.append(f"[green]{wins}[/]/[red]{losses}[/]")
-
-            table.add_row(*row)
-
-        return table
-
-    def get_viewer_url(self, replay_name: str) -> str:
-        return f"http://localhost:8000/client/resources/app/dist/index.html?gameSource=http://localhost:8000/{replay_name}"
-
-def run_match(state: State, map: str, reverse: bool) -> None:
-    player1 = state.player1 if not reverse else state.player2
-    player2 = state.player2 if not reverse else state.player1
-
-    replay_name = f"replays/run-{state.timestamp}-{player1}-vs-{player2}-on-{map}.bc24"
-
-    process = subprocess.run([
-        str(Path(__file__).parent.parent / "gradlew"),
-        "run",
-        "-x", "unpackClient",
-        "-x", "checkForUpdates",
-        f"-PteamA={player1}",
-        f"-PteamB={player2}",
-        f"-Pmaps={map}",
-        f"-PreplayPath={replay_name}"
-    ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    stdout = process.stdout.decode("utf-8")
-
-    if process.returncode != 0:
-        print(f"Gradle exited with status code {process.returncode} after running {player1} vs. {player2} on {map}\n{stdout}")
-        sys.exit(process.returncode)
-
-    lines = stdout.splitlines()
-
-    winner_line = next(line for line in lines if ") wins (" in line)
-    player1_wins = "(A)" in winner_line
-
-    win_condition_line = next(line for line in lines if line.startswith("[server] Reason: "))
-    win_condition = win_condition_line.split(": ", 1)[1]
-
-    state.matches.append(Match(map, reverse, player1_wins, win_condition, replay_name))
-    state.print()
-
-def main() -> None:
-    parser = ArgumentParser(description="Compare the performance of two players.")
-    parser.add_argument("Version10", type=str, help="name of the first player")
-    parser.add_argument("Version9", type=str, help="name of the second player")
-
-    args = parser.parse_args()
-
-    build_proc = subprocess.run([str(Path(__file__).parent.parent / "gradlew"), "build", "-x", "checkForUpdates"])
-    if build_proc.returncode != 0:
-        sys.exit(build_proc.returncode)
-
-    # Based on ENGINE_BUILTIN_MAP_NAMES in https://github.com/battlecode/battlecode24/blob/master/client/src/constants.ts
-    maps = [
-        # Default batch 1
-        "DefaultSmall",
-        "DefaultMedium",
-        "DefaultLarge",
-        "DefaultHuge",
-
-        # Sprint 1
-        "Fossil",
-        "Gear",
-        "Justice",
-        "Mirage",
-        "Money",
-        "MoneyTower",
-        "Racetrack",
-        "Restart",
-        "SMILE",
-        "SaltyPepper",
-        "TargetPractice",
-        "UglySweater",
-        "UnderTheSea",
-        "catface",
-        "gardenworld",
-        "memstore",
-
-        # Sprint 2
+def run_match(bot, map):
+    print("Running {} vs {} on {}".format(currentBot, bot, map))
+    try:
+        outputA = str(subprocess.check_output(['./gradlew', 'run', '-PteamA=' + currentBot, '-PteamB=' + bot, '-Pmaps=' + map]))
+        outputB = str(subprocess.check_output(['./gradlew', 'run', '-PteamA=' + bot, '-PteamB=' + currentBot, '-Pmaps=' + map]))
+    except subprocess.CalledProcessError as exc:
+        print("Status: FAIL", exc.returncode, exc.output)
+        return 'Error'
+    else:
+        winAString = '{} (A) wins'.format(currentBot)
+        winBString = '{} (B) wins'.format(currentBot)
+        loseAString = '{} (B) wins'.format(bot)
+        loseBString = '{} (A) wins'.format(bot)
         
+        numWins = 0
+        
+        gameLengthA = retrieveGameLength(outputA)
+        gameLengthB = retrieveGameLength(outputB)
+        
+        if winAString in outputA:
+            numWins += 1
+        else:
+            if not loseAString in outputA:
+                return 'Error'
+        if winBString in outputB:
+            numWins += 1
+        else:
+            if not loseBString in outputB:
+                return 'Error'
+        return numWinsMapping[numWins] + ' (' + ', '.join([gameLengthA, gameLengthB]) + ')'
 
-        # International and US qualifiers
+
+results = {}
+# Run matches
+for bot, map in matches:
+    # Verify match is valid
+    if not bot in botsSet or not map in mapsSet:
+        errors.append('Unable to parse bot={}, map={}'.format(bot, map))
+    # run run_match.py
     
+    results[(bot, map)] = run_match(bot, map)
 
-        # MIT Newbie and High School
-    
-    ]
+# Construct table
+table = [[results.get((bot, map), 'N/A') for bot in bots] for map in maps]
 
-    console = Console(highlight=False)
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+def replaceWithDictionary(s, mapping):
+    for a, b in mapping.items():
+        s = s.replace(a, b)
+    return s
 
-    state = State(args.player1, args.player2, maps, [], console, timestamp)
-    state.print()
+if emojiMode:
+    table = [[replaceWithDictionary(item, emojiMap) for item in row] for row in table]
 
-    matches = []
-    for map in maps:
-        for reverse in [False, True]:
-            matches.append((state, map, reverse))
-
-    with ThreadPool(6) as pool:
-        pool.starmap(run_match, matches)
-
-if __name__ == "__main__":
-    main()
+# Write to file
+with open('matches-summary.txt', 'w') as f:
+    table = [[''] + bots, [':---:' for i in range(len(bots) + 1)]] + [[map] + row for map, row in zip(maps, table)]
+    for line in table:
+        f.write('| ')
+        f.write(' | '.join(line))
+        f.write(' |')
+        f.write('\n')
+    f.write('\n')
+    for error in errors:
+        f.write(error)
